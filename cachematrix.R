c5d64fcf4d614d372345f23c3f1bc1de81b59246
keilaostos <a01566361@itesm.mx>
2021-08-07 15:00:13 -0600

    testing changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
+## Change1 Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Change 2 Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
