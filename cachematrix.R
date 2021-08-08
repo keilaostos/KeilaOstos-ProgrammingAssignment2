5c40d7a1f4c3af593aa72dacc6e14267fa8ae66b
keilaostos <a01566361@itesm.mx>
2021-08-08 01:14:56 -0600

    example

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Change1 Put comments here that give an overall description of what your
+## Put comments here that give an overall description of what your
 ## functions do
 
-## Change 2 Write a short comment describing this function
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
